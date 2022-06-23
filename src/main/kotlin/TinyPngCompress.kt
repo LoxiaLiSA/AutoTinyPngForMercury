7858ce152fcdabc9adc43d87e9450f202b5a3fdd
LoxiaLiSA <xin@projz.com>
2022-06-23 17:13:40 +0800

    initiate

--- src/main/kotlin/TinyPngCompress.kt
+++ src/main/kotlin/TinyPngCompress.kt
@@ -3,6 +3,10 @@ import com.tinify.Tinify
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 import java.io.File
+import java.text.DecimalFormat
+import kotlin.math.log10
+import kotlin.math.pow
+
 
 interface SomethingNeedToImplement {
 
@@ -29,12 +33,14 @@ class SomethingNeedToImplementSample : SomethingNeedToImplement {
     }
 }
 
-fun main() {
+fun readableFileSize(length: Long): String {
+    if (length <= 0) return "0"
+    val units = arrayOf("B", "kB", "MB", "GB", "TB")
+    val digitGroups = (log10(length.toDouble()) / log10(1024.0)).toInt()
+    return DecimalFormat("#,##0.#").format(length / 1024.0.pow(digitGroups.toDouble())) + " " + units[digitGroups]
+}
 
-    // 设置你想要压缩的文件名, 支持多个
-    val imageFileNameList = listOf(
-        "img_doll.png"
-    )
+fun main() {
 
     val somethingNeedToImplement = SomethingNeedToImplementSample()
     val mercuryPath = somethingNeedToImplement.mercuryProjectPath()
@@ -43,6 +49,34 @@ fun main() {
 
     Tinify.setKey(somethingNeedToImplement.tinyPngApiKey())
 
+
+    // 查找项目中的png文件并按文件大小排序
+    val pngList = mutableListOf<File>()
+    FileUtil.traverseFolder(imageResRootPath) { pngFile ->
+        pngList.add(pngFile)
+    }
+    var total = 0L
+    pngList.sortedBy { it.length() }.forEachIndexed { index, file ->
+        total += file.length()
+        println("${index}, ${file.path}, ${readableFileSize(file.length())}")
+    }
+    println("png资源总大小：${readableFileSize(total)}")
+
+
+    // 选出你想要压缩的文件名, 支持多个
+    val imageFileNameList = listOf<String>(
+//        "img_shoe.png",
+//        "img_alien.png",
+//        "meet_text_intro_image.png",
+//        "meet_voice_intro_image.png",
+//        "meet_light_bg3.png",
+//        "icon_pick_bottle_main.png",
+//        "empty_friend_image.png",
+//        "empty_chat_image.png",
+//        "empty_circle_image.png",
+//        "meet_light_bg1.png",
+//        "meet_light_bg4.png",
+    )
     FileUtil.traverseFolder(imageResRootPath) { pngFile ->
         val fileName = pngFile.name
         if (imageFileNameList.contains(fileName)) {
@@ -51,7 +85,6 @@ fun main() {
         }
     }
 }
-
 
 fun compressImageResource(originImagePath: String, cachePath: String) = runBlocking {
     val source: Source = Tinify.fromFile(originImagePath)
